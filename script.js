c165a245d996ca164ed0e07667e74655712957db
RosvaldasG <rosvaldasggg@gmail.com>
2022-09-07 09:15:03 +0300

    Task-8

---
+++ script.js
@@ -0,0 +1,23 @@
+/* ------------------------------ TASK 8 --------------------------------------------
+Sukurkite konstruktoriaus funkciją "Calculator" (naudokite ES5), kuri gebės sukurti objektus su 4 metodais:
+sum(a, b) - priima du skaičius ir grąžina jų sumą.
+subtraction(a, b) - priima du skaičius ir grąžina jų skirtumą.
+multiplication(a, b) - priima du skaičius ir grąžina jų daugybos rezultatą;
+division(a, b) - priima du skaičius ir grąžina jų dalybos rezultatą;
+------------------------------------------------------------------------------------ */
+
+const a = 9;
+const b = 3;
+
+class Calculator {
+  constructor(a, b) {
+    this.sum = a + b;
+    this.sutraction = a - b;
+    this.multiplication = a * b;
+    this.divsion = a / b;
+  }
+}
+
+const calc = new Calculator(a, b);
+
+console.log(calc);
